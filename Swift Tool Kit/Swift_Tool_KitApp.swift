ba1c40281511abbb148f234b07438afca7fbeba5
Bennet van der Linden <vanderlinden.bennet@gmail.com>
2024-08-20 15:25:54 +0200

    Initial Commit

---
+++ Swift Tool Kit/Swift_Tool_KitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Tool_KitApp.swift
+//  Swift Tool Kit
+//
+//  Created by Bennet van der Linden on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Tool_KitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
