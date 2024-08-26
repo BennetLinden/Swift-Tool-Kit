9ae304e4f104bc78ebd015ef910b06a29f043f64
Bennet van der Linden <vanderlinden.bennet@gmail.com>
2024-08-26 17:36:53 +0200

    Add Network Route

---
+++ Swift Tool Kit/Network/Route.swift
@@ -0,0 +1,97 @@
+//
+//  Route.swift
+//  Swift Tool Kit
+//
+//  Created by Bennet van der Linden on 26/08/2024.
+//
+
+import Alamofire
+import Foundation
+
+struct Route<Parameters>: URLRequestConvertible {
+    private var method: HTTPMethod
+    private var url: any URLConvertible
+    private var parameters: Parameters?
+    private var headers: HTTPHeaders?
+    private var encoder: JSONEncoder?
+
+    typealias RequestModifier = (inout URLRequest) throws -> Void
+    private var requestModifier: RequestModifier?
+
+    init(
+        _ method: HTTPMethod,
+        _ url: any URLConvertible,
+        with parameters: Parameters? = nil,
+        headers: HTTPHeaders? = nil,
+        requestModifier: RequestModifier? = nil
+    ) where Parameters == Alamofire.Parameters {
+        self.method = method
+        self.url = url
+        self.parameters = parameters
+        self.headers = headers
+        self.requestModifier = requestModifier
+    }
+
+    init<T: Encodable>(
+        _ method: HTTPMethod,
+        _ url: any URLConvertible,
+        with parameters: T,
+        encoder: JSONEncoder,
+        headers: HTTPHeaders? = nil,
+        requestModifier: RequestModifier? = nil
+    ) where Parameters == any Encodable {
+        self.method = method
+        self.url = url
+        self.parameters = parameters
+        self.encoder = encoder
+        self.headers = headers
+        self.requestModifier = requestModifier
+    }
+
+    private var parameterEncoder: ParameterEncoder {
+        switch method {
+        case .get, .delete, .head:
+            return URLEncodedFormParameterEncoder.default
+        default:
+            if let encoder {
+                return JSONParameterEncoder(encoder: encoder)
+            } else {
+                return JSONParameterEncoder.default
+            }
+        }
+    }
+
+    private var encoding: ParameterEncoding {
+        switch method {
+        case .get, .head:
+            return URLEncoding(
+                destination: .methodDependent,
+                arrayEncoding: .noBrackets,
+                boolEncoding: .literal
+            )
+        default:
+            return JSONEncoding.default
+        }
+    }
+
+    func asURLRequest() throws -> URLRequest {
+        var request = try URLRequest(
+            url: url,
+            method: method,
+            headers: headers
+        )
+
+        try requestModifier?(&request)
+
+        switch parameters {
+        case let parameters as any Encodable:
+            return try parameterEncoder.encode(parameters, into: request)
+
+        case let parameters as Alamofire.Parameters:
+            return try encoding.encode(request, with: parameters)
+
+        default:
+            return request
+        }
+    }
+}
